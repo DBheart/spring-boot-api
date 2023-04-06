0e05fb926b1fb6958cc88c35df8b41990358c43e
DBHeart <deity98@gmail.com>
2023-04-06 20:16:09 +0900

    first commit

---
+++ springboot/src/test/java/kr/deity/springboot/SpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.deity.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
